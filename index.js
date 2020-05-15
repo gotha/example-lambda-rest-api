0c8e39e59f6c35742c7f5d5851730186f0657467
Hristo Georgiev <h.georgiev@hotmail.com>
2020-05-15 18:03:47 +0300

    Simplify lambda code with data loading middleware

--- index.js
+++ index.js
@@ -26,94 +26,66 @@ const loadData = () => {
   })
 }
 
+app.use((_, res, next) => {
+  loadData()
+    .then(() => next())
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("Internal server error");
+    })
+})
+
 
 app.get('/', function (_, res) {
   res.send("Example REST API");
 });
 
 app.get('/employee', function (_, res) {
-  loadData()
-    .then(() => {
-      res.json(data)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).send("Error")
-    })
+  res.json(data)
 })
 
 app.get('/employee/:id', function (req, res) {
-  loadData()
-    .then(() => {
-      const id = req.params.id || false;
-      const employees = data.filter(i => i.id == id)
-      if (employees.length == 0) {
-        return res.status(404).send("Not found")
-      }
-      res.json(employees[0])
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).send("Error")
-    })
+    const id = req.params.id || false;
+    const employees = data.filter(i => i.id == id)
+    if (employees.length == 0) {
+      return res.status(404).send("Not found")
+    }
+    res.json(employees[0])
 })
 
 
 app.post('/employee', function (req, res) {
-  loadData()
-    .then(() => {
-      const lastID = data.reduce((r, v) => (v.id > parseInt(r)) ? parseInt(v.id) : parseInt(r), 1)
-      newEmployee = req.body
-      newEmployee.id = lastID+1
-      data.push(newEmployee)
-      res.json(newEmployee)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).send("Error")
-    })
+    const lastID = data.reduce((r, v) => (v.id > parseInt(r)) ? parseInt(v.id) : parseInt(r), 1)
+    newEmployee = req.body
+    newEmployee.id = lastID+1
+    data.push(newEmployee)
+    res.json(newEmployee)
 })
 
 app.put('/employee/:id', function (req, res) {
-  loadData()
-    .then(() => {
-      const id = req.params.id || false;
-      for (i = 0; i < data.length; i++) {
-        if (data[i].id == id) {
-          newData = req.body
-          newData.id = id
-          data[i] = newData
-          return res.json(newData)
-        }
+    const id = req.params.id || false;
+    for (i = 0; i < data.length; i++) {
+      if (data[i].id == id) {
+        newData = req.body
+        newData.id = id
+        data[i] = newData
+        return res.json(newData)
       }
-      return res.status(404).send("Not found")
-
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).send("Error")
-    })
+    }
+    return res.status(404).send("Not found")
 })
 
 
 app.delete('/employee/:id', function (req, res) {
-  loadData()
-    .then(() => {
-      const id = req.params.id || false;
-      const newData = [];
-      for (i = 0; i < data.length; i++) {
-        if(data[i].id != id) {
-          newData.push(data[i])
-        }
+    const id = req.params.id || false;
+    const newData = [];
+    for (i = 0; i < data.length; i++) {
+      if(data[i].id != id) {
+        newData.push(data[i])
       }
-      data = newData
-      return res.send("OK")
-
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).send("Error")
-    })
+    }
+    data = newData
+    return res.send("OK")
 })
 
 module.exports.handler = serverless(app);

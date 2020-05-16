8c50cf708b58c7f902258c30f66edab3925ba582
Hristo Georgiev <h.georgiev@hotmail.com>
2020-05-16 20:07:42 +0300

    Reformat code; Add linting rules

--- index.js
+++ index.js
@@ -1,92 +1,92 @@
-
 const fs = require('fs');
 const serverless = require('serverless-http');
-const express = require('express')
+const express = require('express');
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-var data = []
+let data = [];
 
 const loadData = () => {
   return new Promise((resolve, reject) => {
     if (data.length > 0) {
-      console.log("reusing data");
-      return resolve()
+      console.log('reusing data');
+
+      return resolve();
     }
-    console.log("reading data");
-    fs.readFile("./data.json", "utf8", (err, content) => {
-      if (err != null) {
-        return reject(err)
+    console.log('reading data');
+    fs.readFile('./data.json', 'utf8', (err, content) => {
+      if (err) {
+        return reject(err);
       }
-      data = JSON.parse(content)
-      resolve()
+      data = JSON.parse(content);
+      resolve();
     });
 
-  })
-}
+  });
+};
 
 app.use((_, res, next) => {
   loadData()
     .then(() => next())
     .catch(err => {
       console.log(err);
-      res.status(500).send("Internal server error");
-    })
-})
-
-
-app.get('/', function (_, res) {
-  res.send("Example REST API");
+      res.status(500).send('Internal server error');
+    });
 });
 
-app.get('/employee', function (_, res) {
-  res.json(data)
-})
+app.get('/', (_, res) => {
+  res.send('Example REST API');
+});
 
-app.get('/employee/:id', function (req, res) {
-    const id = req.params.id || false;
-    const employees = data.filter(i => i.id == id)
-    if (employees.length == 0) {
-      return res.status(404).send("Not found")
+app.get('/employee', (_, res) => {
+  res.json(data);
+});
+
+app.get('/employee/:id', (req, res) => {
+  const id = req.params.id || false;
+  const employees = data.filter(i => parseInt(i.id) === parseInt(id));
+  if (employees.length === 0) {
+    return res.status(404).send('Not found');
+  }
+  res.json(employees[0]);
+});
+
+app.post('/employee', (req, res) => {
+  const lastID = data.reduce((r, v) => (parseInt(v.id) > parseInt(r)) ? parseInt(v.id) : parseInt(r), 1);
+  const newEmployee = req.body;
+  newEmployee.id = lastID+1;
+  data.push(newEmployee);
+  res.json(newEmployee);
+});
+
+app.put('/employee/:id', (req, res) => {
+  const id = req.params.id || false;
+  for (let i = 0; i < data.length; i++) {
+    if (parseInt(data[i].id) === parseInt(id)) {
+      const newData = req.body;
+      newData.id = id;
+      data[i] = newData;
+
+      return res.json(newData);
     }
-    res.json(employees[0])
-})
+  }
 
+  return res.status(404).send('Not found');
+});
 
-app.post('/employee', function (req, res) {
-    const lastID = data.reduce((r, v) => (v.id > parseInt(r)) ? parseInt(v.id) : parseInt(r), 1)
-    newEmployee = req.body
-    newEmployee.id = lastID+1
-    data.push(newEmployee)
-    res.json(newEmployee)
-})
-
-app.put('/employee/:id', function (req, res) {
-    const id = req.params.id || false;
-    for (i = 0; i < data.length; i++) {
-      if (data[i].id == id) {
-        newData = req.body
-        newData.id = id
-        data[i] = newData
-        return res.json(newData)
-      }
+app.delete('/employee/:id', (req, res) => {
+  const id = req.params.id || false;
+  const newData = [];
+  for (let i = 0; i < data.length; i++) {
+    if (parseInt(data[i].id) !== parseInt(id)) {
+      newData.push(data[i]);
     }
-    return res.status(404).send("Not found")
-})
+  }
+  data = newData;
 
-
-app.delete('/employee/:id', function (req, res) {
-    const id = req.params.id || false;
-    const newData = [];
-    for (i = 0; i < data.length; i++) {
-      if(data[i].id != id) {
-        newData.push(data[i])
-      }
-    }
-    data = newData
-    return res.send("OK")
-})
+  return res.send('OK');
+});
 
 module.exports.handler = serverless(app);
 
